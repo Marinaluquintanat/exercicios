27dbdb01c71fdff978f0e0a6f761d78b64a44229
maluqt <00001120302602sp@al.educacao.sp.gov.br>
2025-05-12 14:22:13 -0300

    nome

--- exercicio_4.py
+++ exercicio_4.py
@@ -2,7 +2,7 @@ def somar_pares(lista):
     total = 0
     for i in lista:
             total += i 
-    media = total //len(lista)
+    media = total / len(lista)
     return media
 
 numeros = [1,2,3,4,5,6]
--- exercicio_5.py
+++ exercicio_5.py
@@ -1,14 +1,28 @@
+from exercicio_4 import somar_pares
+
 def cadastrar_aluno(nome, email, serie, nota_1, nota_2, nota_3): 
     alunos = []
 
+    notas = [nota_1, nota_2, nota_3]
     aluno = {
         "nome": nome,
         "email": email,
         "serie": serie, 
-        "notas": [nota_1, nota_2, nota_3]
+        "media": somar_pares([nota_1, nota_2, nota_3])
     }
     alunos.append(aluno)
-    return alunos 
+    
+    return alunos
+print(cadastrar_aluno("marina", "marinaluiza@gmail.com", "2TB", 8,9,9)) 
+
+
+
+
+
+
+
+
+
 
 
 
