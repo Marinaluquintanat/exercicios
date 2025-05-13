affe6ee0bcf995b1f9953b99db9e637819118496
maluqt <00001120302602sp@al.educacao.sp.gov.br>
2025-05-13 08:56:16 -0300

    nome

--- exercicio_5.py
+++ exercicio_5.py
@@ -1,19 +1,44 @@
-from exercicio_4 import somar_pares
+from exercicio04 import somar_pares
 
-def cadastrar_aluno(nome, email, serie, nota_1, nota_2, nota_3): 
-    alunos = []
+alunos = []
 
-    notas = [nota_1, nota_2, nota_3]
+def obter_dados_aluno():
+    nome = input("digite o nome do aluno")
+    email = input("digite o email do aluno")
+    serie = input("qual a serie do aluno")
+    nota01 = int(input("nota01"))
+    nota02 = int(input("nota02"))
+    nota03 = int(input("nota03"))
+    return cadastrar_aluno(nome, email, serie, nota01, nota02, nota03)
+
+def cadastrar_aluno(nome, email, serie, nota01=0, nota02=0, nota03=0):
+
+
+    notas =  [nota01, nota02, nota03]
+
     aluno = {
         "nome": nome,
         "email": email,
-        "serie": serie, 
-        "media": somar_pares([nota_1, nota_2, nota_3])
+        "serie": serie,
+        "media": somar_pares([nota01, nota02, nota03])
     }
     alunos.append(aluno)
-    
+
     return alunos
-print(cadastrar_aluno("marina", "marinaluiza@gmail.com", "2TB", 8,9,9)) 
+
+obter_dados_aluno()
+
+def mostrar_dados_alunos(dados_alunos):
+    for aluno in dados_alunos:
+        print(f"nome do aluno: {aluno["nome"]}")
+    return
+
+mostrar_dados_alunos(alunos)
+
+
+
+
+
 
 
 
